be47f4f2702ae785bf4add9ea043e5de34c6594d
vivi88605 <VIVI88605@gmail.com>
2023-05-06 17:30:18 +0800

    add moveList at app.js render movies from movieList

--- app.js
+++ app.js
@@ -13,13 +13,42 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   //create variable to store movie1
-  const movie1 = {
-    id: 1,
-    title: 'Jurassic World: Fallen Kingdom',
-    image: 'https://movie-list.alphacamp.io/posters/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg',
-  }
-  const numberList = [1, 2, 3, 4, 5, 6, 7]
-  res.render('index', { movie: movie1, numbers: numberList })
+  const movieList = [
+    {
+      id: 1,
+      title: 'Jurassic World: Fallen Kingdom',
+      image: 'https://movie-list.alphacamp.io/posters/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg'
+    }, {
+      id: 2,
+      title: 'Ant-Man and the Wasp',
+      image: 'https://movie-list.alphacamp.io/posters/rv1AWImgx386ULjcf62VYaW8zSt.jpg'
+    }, {
+      id: 3,
+      title: 'Thor: Ragnaro',
+      image: 'https://movie-list.alphacamp.io/posters/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg'
+    }, {
+      id: 4,
+      title: 'Avengers: Infinity War',
+      image: 'https://movie-list.alphacamp.io/posters/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg'
+    }, {
+      id: 5,
+      title: 'Mission: Impossible - Fallout',
+      image: 'https://movie-list.alphacamp.io/posters/80PWnSTkygi3QWWmJ3hrAwqvLnO.jpg'
+    }, {
+      id: 6,
+      title: 'Incredibles 2',
+      image: 'https://movie-list.alphacamp.io/posters/x1txcDXkcM65gl7w20PwYSxAYah.jpg'
+    }, {
+      id: 7,
+      title: 'Fifty Shades Freed',
+      image: 'https://movie-list.alphacamp.io/posters/jjPJ4s3DWZZvI4vw8Xfi4Vqa1Q8.jpg'
+    }, {
+      id: 8,
+      title: 'The First Purge',
+      image: 'https://movie-list.alphacamp.io/posters/2slvblTroiT1lY9bYLK7Amigo1k.jpg'
+    }
+  ]
+  res.render('index', { movies: movieList })
 })
 
 app.listen(port, () => {
